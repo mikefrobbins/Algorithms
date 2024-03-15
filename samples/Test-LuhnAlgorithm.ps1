b0bec3e6689461880907b5d8463a407d3a3d700e
Mike F. Robbins <mikefrobbins@users.noreply.github.com>
2024-03-14 19:57:37 -0500

    Added function to test numbers with luhn algorithm

---
+++ samples/Test-LuhnAlgorithm.ps1
@@ -0,0 +1,28 @@
+﻿function Test-LuhnAlgorithm {
+    [CmdletBinding()]
+    param (
+        [string]$Number
+    )
+
+    $digits = [char[]]$number | ForEach-Object {$_ - 48}
+
+    $result = for ($i = $digits.Length - 2; $i -ge 0; $i--) {
+        $digit = $digits[$i]
+        if ($i % 2 -eq 0) {
+            $digit *= 2
+            if ($digit -gt 9) {
+                $digit -= 9
+            }
+        }
+        $digit
+    }
+
+    $sum = $result | Measure-Object -Sum | Select-Object -ExpandProperty Sum
+
+    if (10 - $sum % 10 -eq $digits[-1]) {
+        $true
+    } else {
+        $false
+    }
+
+}
